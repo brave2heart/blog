1410cee85115504dbd7804afc71bd410fa2321af
weihao <weihao@zlg.cn>
2019-10-30 09:23:30 +0800

    新增路径文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,44 @@
 module.exports = {
-  title: '个人主页',
-  description: '姜帅杰的博客',
+  title: 'zwslib & template',
+  description: 'Reusable Framework',
   head: [
-    ['link', { rel: 'icon', href: '/img/logo.ico' }],
-    ['link', { rel: 'manifest', href: '/manifest.json' }],
-  ]
+    // 注入到当前页面的 HTML <head> 中的标签
+    [
+      'link',
+      {
+        rel: 'icon',
+        href: '/logo.jpg'
+      }
+    ], // 增加一个自定义的 favicon(网页标签的图标)
+    [
+      'link',
+      {
+        rel: 'manifest',
+        href: '/photo.jpg'
+      }
+    ],
+    [
+      'link',
+      {
+        rel: 'apple-touch-icon',
+        href: '/photo.jpg'
+      }
+    ]
+  ],
+  base: '/', // 这是部署到github相关的配置
+  markdown: {
+    lineNumbers: false // 代码块显示行号
+  },
+  themeConfig: {
+    nav: [
+      // 导航栏配置
+      {
+        text: '在线模板',
+        link: 'https://zlab.zlgcloud.com/zwslib/template/index.html'
+      }
+    ],
+    sidebar: 'auto', // 侧边栏配置
+    sidebarDepth: 2, // 侧边栏显示2级
+    sidebar: ['/md/zwslib.md', '/md/template.md']
+  }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,16 +1,14 @@
 ---
 home: true
-heroImage: /hero.png
-heroText: Hero 标题
-tagline: Hero 副标题
+heroImage: https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1565685573899&di=7d6712049b61b6167115e51e688f64eb&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F0168725aefcac0a801206aba354608.gif
 actionText: 快速上手 →
-actionLink: /zh/guide/
+actionLink: /md/zwslib
 features:
-- title: 简洁至上
-  details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
-- title: Vue驱动
-  details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。
-- title: 高性能
-  details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
+  - title: zwslib
+    details: 基于vue和element-ui开发。
+  - title: template
+    details: 基于zwslib库开发的项目模板，快速实现公司业务。
+  - title: zwslib & template
+    details: zwslib库和template模板的说明文档。
 footer: MIT Licensed | Copyright © 2018-present Evan You
 ---
