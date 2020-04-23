8d63c1eccaf4bfca1ce97b0835305dea99012bc5
weihao <1120575939@qq.com>
2020-04-23 22:49:07 +0800

    update blog style

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,70 +1,84 @@
 module.exports = {
-  title: 'WEI HAO',
-  description: '个人博客',
-  head: [
-    // 注入到当前页面的 HTML <head> 中的标签
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+  "title": "",
+  "description": "",
+  "dest": "public",
+  "head": [
     [
-      'link',
+      "link",
       {
-        rel: 'icon',
-        href: '/logo.jpg'
-      }
-    ], // 增加一个自定义的 favicon(网页标签的图标)
-    [
-      'link',
-      {
-        rel: 'manifest',
-        href: '/photo.jpg'
+        "rel": "icon",
+        "href": "/favicon.ico"
       }
     ],
     [
-      'link',
+      "meta",
       {
-        rel: 'apple-touch-icon',
-        href: '/photo.jpg'
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
       }
     ]
   ],
-  base: '/', // 这是部署到github相关的配置
-  markdown: {
-    lineNumbers: false // 代码块显示行号
-  },
-  themeConfig: {
-    // logo: '/assets/img/logo.png',
-    nav: [
+  "theme": "reco",
+  "themeConfig": {
+    "nav": [
       {
-        text: 'kotlin',
-        link: '/md/kotlin.md'
+        "text": "Home",
+        "link": "/",
+        "icon": "reco-home"
       },
       {
-        text: 'VuePress',
-        link: 'https://vuepress.vuejs.org/zh/'
+        "text": "TimeLine",
+        "link": "/timeline/",
+        "icon": "reco-date"
       },
       {
-        text: 'GitHub',
-        link: 'https://github.com/dpoyweihao'
+        "text": "Contact",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "GitHub",
+            "link": "https://github.com/recoluan",
+            "icon": "reco-github"
+          }
+        ]
       }
     ],
-    sidebar: 'auto', // 侧边栏配置
-    sidebarDepth: 2, // 侧边栏显示2级
-    sidebar: {
-      // {
-      //   title: '专题',   // 必要的
-      //   path: '/foo/',      // 可选的, 应该是一个绝对路径
-      //   collapsable: true, // 可选的, 默认值是 true,
-      //   sidebarDepth: 1,    // 可选的, 默认值是 1
-      //   children: [
-      //     '/md/Kotlin.md'
-      //   ]
-      // },
-      '/md/kotlin.md': [
-        {
-          title: '基础',
-          collapsable: false
-        }
-      ]
-    }
+    "type": "blog",
+    "blogConfig": {
+      "category": {
+        "location": 2,
+        "text": "Category"
+      },
+      "tag": {
+        "location": 3,
+        "text": "Tag"
+      }
+    },
+    "friendLink": [
+      {
+        "title": "午后南杂",
+        "desc": "Enjoy when you can, and endure when you must.",
+        "email": "1156743527@qq.com",
+        "link": "https://www.recoluan.com"
+      },
+      {
+        "title": "vuepress-theme-reco",
+        "desc": "A simple and beautiful vuepress Blog & Doc theme.",
+        "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+        "link": "https://vuepress-theme-reco.recoluan.com"
+      }
+    ],
+    "logo": "/logo.png",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "sidebar": "auto",
+    "lastUpdated": "Last Updated",
+    "author": "",
+    "authorAvatar": "/avatar.png",
+    "record": "xxxx",
+    "startYear": "2017"
+  },
+  "markdown": {
+    "lineNumbers": true
   }
 }
-
--- docs/README.md
+++ docs/README.md
@@ -1,20 +1,25 @@
 ---
 home: true
-heroImage: https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1565685573899&di=7d6712049b61b6167115e51e688f64eb&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F0168725aefcac0a801206aba354608.gif
-actionText: 快速上手 →
-actionLink: /md/kotlin
+# heroImage: /hero.png
+# heroImageStyle: {
+#   maxWidth: '600px',
+#   width: '100%',
+#   display: block,
+#   margin: '9rem auto 2rem',
+#   background: '#fff',
+#   borderRadius: '1rem',
+# }
+bgImageStyle: {
+  height: '450px'
+}
+isShowTitleInHome: false
+actionText: Guide
+actionLink: /views/other/guide
 features:
-  - title: Flutter
-    details: Flutter是Google的移动UI框架，可以快速在iOS和Android上构建高质量的原生用户界面。
-  - title: Vue
-    details: Vue是一套用于构建用户界面的渐进式框架。与其它大型框架不同的是，Vue 被设计为可以自底向上逐层应用。
-  - title: Kotlin
-    details: Kotlin 是一种现代的静态设置类型编程语言，可以提高开发者的工作效率，并提升开发者的工作愉悦度。
-  - title: Android Jetpack
-    details: Jetpack 是一套库、工具和指南，可帮助开发者更轻松地编写优质应用。这些组件可帮助您遵循最佳做法、让您摆脱编写样板代码的工作并简化复杂任务，以便您将精力集中放在所需的代码上。
-  - title: 微信小程序
-    details: 微信小程序提供了一个简单、高效的应用开发框架和丰富的组件及API，帮助开发者在微信中开发具有原生 APP 体验的服务。
-  - title: Weex
-    details: Weex 致力于使开发者能基于通用跨平台的 Web 开发语言和开发经验，来构建 Android、iOS 和 Web 应用。简单来说，在集成了 WeexSDK 之后，你可以使用 JavaScript 语言和前端开发经验来开发移动应用。
-footer: MIT Licensed | Copyright © 2019-present Evan You
+- title: Yesterday
+  details: 开发一款看着开心、写着顺手的 vuepress 博客主题
+- title: Today
+  details: 希望帮助更多的人花更多的时间在内容创作上，而不是博客搭建上
+- title: Tomorrow
+  details: 希望更多的爱好者能够参与进来，帮助这个主题更好的成长
 ---
